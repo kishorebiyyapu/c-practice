26c9b7529e7f9c91546fef823a6f3be87c830768
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-19 19:19:39 +0530

    Create prog23.c

---
+++ prog23.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a[100],temp,n,i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	temp=a[1];
+	for(i=2;i<=n;i++)
+	{
+		if(a[i]<temp)
+		     temp=a[i];
+	}
+	printf("%d",temp);
+	return 0;
+}
