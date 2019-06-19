275bd6f0f67150711e701d5e1b635205bd0ec7d7
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-19 19:21:40 +0530

    Create prog24.c

---
+++ prog24.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a[100],temp,n,i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<=n;i++)
+	{
+	   for(j=1;j<=n;j++)
+	   {
+	   	if(a[j]>a[j+1])
+	   	{
+	   		temp=a[j];
+	   		a[j]=a[j+1];
+	   		a[j+1]=temp;
+		   }
+	   }
+	}
+	for(i=1;i<=n;i++)
+	{
+	printf("%d ",a[i]);
+    }
+	return 0;
+}
