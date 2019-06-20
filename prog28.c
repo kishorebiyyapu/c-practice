24c14844d0ea42269f4ee89039854ca976717d4c
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-20 19:20:12 +0530

    Create prog28.c

---
+++ prog28.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a[100],n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d",a[i],i);
+	}
+	return 0;
+}
