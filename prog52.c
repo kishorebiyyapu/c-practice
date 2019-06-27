364d0109f1369272116a5ad03fd2e045b8e3fda9
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-06-27 20:40:24 +0530

    Create prog52.c

---
+++ prog52.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	if(N==1)
+	    printf("One");
+	else if(N==2)
+	    printf("Two");
+	else if(N==3)
+	    printf("Three");
+	else if(N==4)
+	    printf("FOur");
+	else if(N==5)
+	    printf("Five");
+	else if(N==6)
+	    printf("Six");
+	else if(N==2)
+	    printf("Seven");
+	else if(N==8)
+	    printf("Eight");
+	else if(N==9)
+	    printf("Nine");
+	else if(N==10)
+	    printf("Ten");
+	return 0;								    
+		 
+}
